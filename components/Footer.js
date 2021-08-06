4faa87d0b03d7e9c08517d7a951c6f9218e9b175
Sizan Rana <sizanrana@Sizans-MacBook-Pro.local>
2021-08-06 22:11:40 +0545

    this is search page update

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-10 bg-gray-100">
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-10 bg-gray-100 text-gray-600s">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
         <p>Airbnb</p>
