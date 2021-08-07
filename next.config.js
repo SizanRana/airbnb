bb6878037ed6c79be8fd6eda664918a759c59a9e
Sizan Rana <sizanrana@Sizans-MacBook-Pro.local>
2021-08-07 15:24:57 +0545

    added map functionality

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoic2l6YW5yYW5hMjAwMCIsImEiOiJja3MxYzJiZzQwMXZiMnVxbWdlYmh1bG0wIn0.6iWT3bUPL3SCTUQDsRy1hw",
+  },
 };
